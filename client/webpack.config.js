4d45bfe883278e7cd5b77b2516cb2b0cb08b2646
Bruno Bulnes <bulnes.bruno@gmail.com>
2020-08-16 00:32:54 -0300

    funcionalidade: adicionando o plugin html-webpack-plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,21 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   mode: "development",
+  devServer: {
+    open: true,
+    contentBase: "dist"
+  },
   entry: "./src/componentes/lista/listagem-cliente.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/clientes.html",
+      filename: "index.html"
+    })
+  ]
 };
